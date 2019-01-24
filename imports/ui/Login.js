8ae759cd64e1f3e5d32e9d663f0b83a6a225a60a
leeivana <lee.ivana@hotmail.com>
2019-01-24 02:13:38 -0500

    Added submit logic for login form

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,13 +1,21 @@
-import React from 'react'; 
+import React, { Component } from 'react'; 
 
-const Login = () => {
-  return (
-    <form>
-      <input type="email" name="loginEmail" />
-      <input type="password" name="loginPassword" />
-      <input type="submit" value="Login" />
-    </form>
-  )
+class Login extends Component {
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const email = event.target.loginEmail.value;
+    const password = event.target.loginPassword.value;
+    console.log(email, password);
+  }
+  render(){
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="email" name="loginEmail" />
+        <input type="password" name="loginPassword" />
+        <input type="submit" value="Login" />
+      </form>
+    )
+  }
 }
 
 export default Login; 
