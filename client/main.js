422de8e464b7bf207ac480e2401077b39be81c1c
leeivana <lee.ivana@hotmail.com>
2019-01-27 13:47:55 -0500

    Added code to create Teams table in db

--- client/main.js
+++ client/main.js
@@ -4,6 +4,7 @@ import { render } from 'react-dom';
 import './routing.jsx';
 
 import App from '../imports/ui/App.js';
+import '../imports/api/teams';
 
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
